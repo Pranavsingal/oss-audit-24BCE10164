f525156f2a96b728a0918187d1355fb49ee75735
Pranav Singal <173361523+Pranavsingal@users.noreply.github.com>
2026-03-28 14:17:21 +0000

    Initial OSS Project commit

---
+++ script1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script 1: System Identity Report
+
+echo "================================"
+echo " Open Source Audit — Pranav"
+echo "================================"
+
+echo "Kernel  : $(uname -r)"
+echo "User    : $(whoami)"
+echo "Home    : $HOME"
+echo "Uptime  : $(uptime -p)"
+echo "Date    : $(date)"
+
+echo "Distro  : $(lsb_release -d | cut -f2)"
+
+echo "License : GNU GPL (Linux Kernel)"
+
